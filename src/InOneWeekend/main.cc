68f9fe8006b01209f8e01853c1c3cb15ebfdb4bf
Jacob Goldman <jacobmgoldman@icloud.com>
2023-02-20 14:54:16 -0500

    fixed random seed and lighter simulation for benchmarking

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -94,14 +94,15 @@ hittable_list random_scene() {
 
 
 int main() {
+    srand(42);
 
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 10;
-    const int max_depth = 50;
+    const int samples_per_pixel = 2;
+    const int max_depth = 25;
 
     // World
 
